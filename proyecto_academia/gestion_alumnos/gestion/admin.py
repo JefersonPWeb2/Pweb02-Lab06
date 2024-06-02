34ce3675a2a2db2a8465fd7db962ee1fca874cd9
Jeferson Basurco <132604488+JefersonPWeb2@users.noreply.github.com>
2024-06-01 20:48:39 -0500

    Delete proyecto_academia directory

--- proyecto_academia/gestion_alumnos/gestion/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Alumno, Curso, NotaAlumnoPorCurso
-
-admin.site.register(Alumno)
-admin.site.register(Curso)
-admin.site.register(NotaAlumnoPorCurso)
